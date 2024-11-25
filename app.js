79020c2009fb3fd45bca4d9f38044fbf82fd16c3
Dariusz Okoński <darek200180@gmail.com>
2024-11-25 18:24:05 +0100

    add mongo connection

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
+// bootstrap 4.6
 const express = require('express');
 const { engine } = require('express-handlebars');
+const mongoose = require('mongoose');
+
+// db connection
+mongoose.connect('mongodb://127.0.0.1:27017/vidjot-dev');
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('MongoDB connected...');
+});
+// ====================
 
 const app = express();
 
