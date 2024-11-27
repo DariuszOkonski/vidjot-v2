acf2db9a323ed9f6a3d3f267725a0d09e1948c79
Dariusz Okoński <darek200180@gmail.com>
2024-11-27 17:54:04 +0100

    add idea form

--- app.js
+++ app.js
@@ -33,6 +33,10 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 
+app.get('/ideas/add', (req, res) => {
+  res.render('ideas/add');
+});
+
 const port = 5000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}...`);
