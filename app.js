ac83744ae506e7e3418d7a0fa6f285d31d7744b5
Dariusz Okoński <darek200180@gmail.com>
2024-11-27 18:06:34 +0100

    add video ideas

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // bootstrap 4.6
 const express = require('express');
 const { engine } = require('express-handlebars');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 // db connection
@@ -22,6 +23,9 @@ app.engine('handlebars', engine());
 app.set('view engine', 'handlebars');
 app.set('views', './views');
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
   const title = 'Welcome';
   res.render('index', {
@@ -35,6 +39,27 @@ app.get('/about', (req, res) => {
 
 app.get('/ideas/add', (req, res) => {
   res.render('ideas/add');
+});
+
+app.post('/ideas', (req, res) => {
+  let errors = [];
+
+  if (!req.body.title) {
+    errors.push({ text: 'Please add a title' });
+  }
+  if (!req.body.details) {
+    errors.push({ text: 'Please add some details' });
+  }
+
+  if (errors.length > 0) {
+    res.render('ideas/add', {
+      errors,
+      title: req.body.title,
+      details: req.body.details,
+    });
+  } else {
+    res.send('passed');
+  }
 });
 
 const port = 5000;
