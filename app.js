777af9f694509df6bac9cca8b8ec435fd08e5a49
Dariusz Okoński <darek200180@gmail.com>
2024-11-30 14:07:09 +0100

    add routes

--- app.js
+++ app.js
@@ -16,10 +16,11 @@ db.once('open', () => {
 });
 // ====================
 
-require('./models/Idea');
-const Idea = mongoose.model('ideas');
-
 const app = express();
+
+//load routes
+const ideas = require('./routes/ideas');
+const users = require('./routes/users');
 
 // hbs middleware
 app.engine('handlebars', engine());
@@ -60,73 +61,9 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 
-app.get('/ideas', (req, res) => {
-  Idea.find({})
-    .lean()
-    .sort({ date: 'desc' })
-    .then((ideas) => {
-      res.render('ideas/index', { ideas: ideas });
-    });
-});
-
-app.get('/ideas/add', (req, res) => {
-  res.render('ideas/add');
-});
-
-app.get('/ideas/edit/:id', (req, res) => {
-  Idea.findOne({ _id: req.params.id })
-    .lean()
-    .then((idea) => {
-      res.render('ideas/edit', { idea });
-    });
-});
-
-app.post('/ideas', (req, res) => {
-  let errors = [];
-
-  if (!req.body.title) {
-    errors.push({ text: 'Please add a title' });
-  }
-  if (!req.body.details) {
-    errors.push({ text: 'Please add some details' });
-  }
-
-  if (errors.length > 0) {
-    res.render('ideas/add', {
-      errors,
-      title: req.body.title,
-      details: req.body.details,
-    });
-  } else {
-    const newUser = {
-      title: req.body.title,
-      details: req.body.details,
-    };
-    new Idea(newUser).save().then((idea) => {
-      req.flash('success_msg', 'Video Idea added');
-      res.redirect(`/ideas`);
-    });
-  }
-});
-
-app.put('/ideas/:id', (req, res) => {
-  Idea.findOne({ _id: req.params.id }).then((idea) => {
-    idea.title = req.body.title;
-    idea.details = req.body.details;
-
-    idea.save().then((idea) => {
-      req.flash('success_msg', 'Video idea updated');
-      res.redirect('/ideas');
-    });
-  });
-});
-
-app.delete('/ideas/:id', (req, res) => {
-  Idea.deleteOne({ _id: req.params.id }).then(() => {
-    req.flash('success_msg', 'Video Idea removed');
-    res.redirect('/ideas');
-  });
-});
+// use routes
+app.use('/ideas', ideas);
+app.use('/users', users);
 
 const port = 5000;
 app.listen(port, () => {
