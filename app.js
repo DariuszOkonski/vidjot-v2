7793cb16e2420cba4f77d165dc26eaf6ffcef2ef
Dariusz Okoński <darek200180@gmail.com>
2024-12-01 12:01:06 +0100

    problems with login

--- app.js
+++ app.js
@@ -45,6 +45,10 @@ app.use(
   })
 );
 
+// passport middleware, has to be after express session
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(flash());
 
 //global variables
@@ -52,6 +56,7 @@ app.use(function (req, res, next) {
   res.locals.success_msg = req.flash('success_msg');
   res.locals.error_msg = req.flash('error_msg');
   res.locals.error = req.flash('error');
+  res.locals.user = req.user || null;
   next();
 });
 
--- config/passport.js
+++ config/passport.js
@@ -8,8 +8,53 @@ const User = mongoose.model('users');
 module.exports = function (passport) {
   passport.use(
     new LocalStrategy({ usernameField: 'email' }, (email, password, done) => {
-      console.log('email: ', email);
-      console.log('password: ', password);
+      // match user
+      User.findOne({ email }).then((user) => {
+        if (!user) {
+          return done(null, false, { message: 'No User Found' });
+        }
+
+        console.log('user: ', user);
+
+        // match password
+        bcrypt.compare(password, user.password, (err, isMatch) => {
+          if (err) throw err;
+
+          console.log('isMatch: ', isMatch);
+
+          if (isMatch) {
+            return done(null, user);
+          } else {
+            return done(null, false, { message: 'Password Incorrect' });
+          }
+        });
+      });
     })
   );
+
+  // passport.use(
+  //   new LocalStrategy(function (email, password, done) {
+  //     User.findOne({ email }, function (err, user) {
+  //       if (err) {
+  //         return done(err);
+  //       }
+
+  //       if (!user) {
+  //         return done(null, false);
+  //       }
+
+  //       return done(null, user);
+  //     });
+  //   })
+  // );
+
+  passport.serializeUser(function (user, done) {
+    done(null, user.id);
+  });
+
+  passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+      done(err, user);
+    });
+  });
 };
